8651f68cad16cadd4f3d0f11ab2e742e547caeb7
Matias Cudich <mcudich@gmail.com>
2016-09-01 17:17:16 -0700

    Tweak app nodes

--- Example/Example/App.swift
+++ Example/Example/App.swift
@@ -9,45 +9,22 @@
 import Foundation
 import TemplateKit
 
-struct AppState {
-  private(set) var counter = 0
-
-  mutating func increment() {
-    counter += 1
-  }
-}
-
 class App: Node {
   var root: Node?
   var renderedView: UIView?
   var properties: [String : Any]
-  public var state: Any? = AppState()
+  public var state: Any?
   var calculatedFrame: CGRect?
   public var eventTarget = EventTarget()
-
-  var appState: AppState {
-    set {
-      state = newValue
-    }
-    get {
-      return state as! AppState
-    }
-  }
 
   required init(properties: [String : Any]) {
     self.properties = properties
   }
 
   func build(completion: (Node) -> Void) {
-    let app = Box(properties: ["width": CGFloat(320), "height": CGFloat(500), "paddingTop": CGFloat(60)]) {
-      [Text(properties: ["text": "Increment", "onTap": incrementCounter]),
-       Text(properties: ["text": "\(appState.counter)"])]
-    }
-    completion(app)
-  }
-
-  private func incrementCounter() {
-    appState.increment()
-    update()
+    completion(
+      Box(properties: ["width": CGFloat(320), "height": CGFloat(500), "paddingTop": CGFloat(60)]) {
+        [Counter(properties: [:])]
+    })
   }
 }
---
+++ Example/Example/Counter.swift
@@ -0,0 +1,53 @@
+//
+//  App.swift
+//  Example
+//
+//  Created by Matias Cudich on 9/1/16.
+//  Copyright © 2016 Matias Cudich. All rights reserved.
+//
+
+import Foundation
+import TemplateKit
+
+class Counter: Node {
+  struct State {
+    private(set) var counter = 0
+
+    mutating func increment() {
+      counter += 1
+    }
+  }
+
+  var root: Node?
+  var renderedView: UIView?
+  var properties: [String : Any]
+  public var state: Any? = State()
+  var calculatedFrame: CGRect?
+  public var eventTarget = EventTarget()
+
+  var counterState: State {
+    set {
+      state = newValue
+    }
+    get {
+      return state as! State
+    }
+  }
+
+  required init(properties: [String : Any]) {
+    self.properties = properties
+  }
+
+  func build(completion: (Node) -> Void) {
+    completion(
+      Box(properties: [:]) {
+        [Text(properties: ["text": "Increment", "onTap": incrementCounter]),
+         Text(properties: ["text": "\(counterState.counter)"])]
+    })
+  }
+
+  private func incrementCounter() {
+    counterState.increment()
+    update()
+  }
+}
