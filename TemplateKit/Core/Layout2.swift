191409cc76a290fe76baad426d9ef14f86ad3239
Matias Cudich <mcudich@gmail.com>
2016-09-09 13:38:15 -0700

    Added CSSLayout

---
+++ TemplateKit/Core/Layout2.swift
@@ -0,0 +1,16 @@
+//
+//  Layout2.swift
+//  TemplateKit
+//
+//  Created by Matias Cudich on 9/9/16.
+//  Copyright © 2016 Matias Cudich. All rights reserved.
+//
+
+import Foundation
+import CSSLayout
+
+class CSSNode {
+  func doStuff() {
+    
+  }
+}
